677fce3e1ccc6a864ea3ad9203eb02d96ae8badf
unknown <mukul7883@gmail.com>
2021-12-03 19:31:59 +0530

    final push

--- controller/timeController.js
+++ controller/timeController.js
@@ -1,28 +1,31 @@
 const moment = require("moment");
 const triggerFunction = async (req, res) => {
     try {
-        let eventArray = req.body.eventQueue;
+        let { eventArray } = req.body;
         if (!eventArray) {
-            return res.status(400).send({ status: "failure", msg: "input the time events" });
+            return res.status(400).send({ status: "failure", message: "input the time events" });
         }
-        for (let i = 0; i < eventArray.length; i++) {
-            const givenDate = moment(new Date(`${eventArray[i].dateTime}`)).format(
-                "Y-M-D H:m:s"
-            );
-            const date = new Date(givenDate);
-            const currentDate = new Date();
-          
-            let diff = date - currentDate;
-            let delayTime = diff + eventArray[i].text.length * 1000;
 
-            setTimeout(() => {
-                console.log(eventArray[i].text.split("").reverse().join(""))
-            }, delayTime);
+        for (let i = 0; i < eventArray.length; i++) {
+            let inputDate = moment(new Date(`${eventArray[i].dateTime}`)).format("YYYY-MM-DD HH:mm:ss.SSS");
+            inputDate = new Date(inputDate);
+            const currentDate = new Date();
+
+            let executeAfter = inputDate - currentDate;
+
+            if (executeAfter >= 0) {
+                setTimeout(() => {
+                    console.log(eventArray[i].text.split("").reverse().join(""))
+                }, executeAfter + eventArray[i].text.length * 1000);
+            }
+            else {
+                console.log(`event ${i + 1} expires`)
+            }
         }
-        res.status(200).send({ status: "success", msg: "all job done" })
-    } 
-    catch (err) {
-        res.status(500).send({ status: "failure", err: err.message });
+        res.status(200).send({ status: "success", message: "schedled" })
+    }
+    catch (error) {
+        res.status(500).send({ status: "failure", error: error.message });
     }
 };
 module.exports.triggerFunction = triggerFunction;
